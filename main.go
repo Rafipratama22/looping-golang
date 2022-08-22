d44c889d71ad1bad3ca1ecef55b6c6d87a046ef2
rafi pratama <rafipratama876@gmail.com>
2022-08-22 20:12:06 +0700

    print people from class A

--- main.go
+++ main.go
@@ -6,8 +6,13 @@ func main() {
 	for i := 0; i <= 10; i++ {
 		if i % 2 == 1 {
 			fmt.Println(i, "ganjil")
-		} else if i % 2 == 0 {
+		} else {
 			fmt.Println(i, "genap")
 		}
+	}
+
+	names := []string{"Fadli", "David", "Fisa", "Teguh", "Fajar", "Irfan", "Edwin", "Hans", "Jaka", "Wicaksana"}
+	for _, n := range names {
+		fmt.Println(n)
 	}
 }
