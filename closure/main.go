f57e4d05fe19d34e7cc1ec57cc798f306a962668
rafi pratama <rafipratama876@gmail.com>
2022-08-24 19:47:48 +0700

    struct and pointer

--- closure/main.go
+++ closure/main.go
@@ -1,17 +1,26 @@
-package closure
+package main
 
 import "fmt"
 
+type nameStruct struct {
+	named string
+}
+
 func main() {
-	var pointName []*string
+	// var pointName []*string
+	var pointName []*nameStruct
 	names := []string{"Fadli", "David", "Fisa", "Teguh", "Fajar", "Irfan", "Edwin", "Hans", "Jaka", "Wicaksana"}
 	for _, name := range names {
-		pointName = append(pointName, &name)
+		// pointName = append(pointName, &name)
+		named := nameStruct{
+			named: name,
+		}
+		pointName = append(pointName, &named)
 	}
 	// pointName := []*string{&name1, &name2, &name3, &name4, &name5}
-	funName := func(pointName []*string) {
+	funName := func(pointName []*nameStruct) {
 		for _, point := range pointName {
-			fmt.Println(*point)
+			fmt.Println(point.named)
 		}
 	}
 	funName(pointName)
