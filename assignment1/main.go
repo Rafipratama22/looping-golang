c33ccdc9dbccf44196f64be3b6893d4e501435eb
rafi pratama <rafipratama876@gmail.com>
2022-08-24 20:36:29 +0700

    assignment 1

---
+++ assignment1/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type student struct {
+	name string
+	alamat string
+	pekerjaan string
+	reason string
+}
+
+func main() {
+	// names := []string{"Fadli", "David", "Fisa", "Teguh", "Fajar", "Irfan", "Edwin", "Hans", "Jaka", "Wicaksana"}
+	students := []student{
+		{
+			name: "Fadli",
+			alamat: "jakarta",
+			pekerjaan: "developer",
+			reason: "suka aja",
+		},
+		{
+			name: "David",
+			alamat: "jakarta",
+			pekerjaan: "developer",
+			reason: "suka aja",
+		},
+		{
+			name: "Fisa",
+			alamat: "jakarta",
+			pekerjaan: "developer",
+			reason: "suka aja",
+		},
+		{
+			name: "Teguh",
+			alamat: "jakarta",
+			pekerjaan: "developer",
+			reason: "suka aja",
+		},
+		{
+			name: "Fajar",
+			alamat: "jakarta",
+			pekerjaan: "developer",
+			reason: "suka aja",
+		},
+		{
+			name: "Irfan",
+			alamat: "jakarta",
+			pekerjaan: "developer",
+			reason: "suka aja",
+		},
+		{
+			name: "Edwin",
+			alamat: "jakarta",
+			pekerjaan: "developer",
+			reason: "suka aja",
+		},
+		{
+			name: "Hans",
+			alamat: "jakarta",
+			pekerjaan: "developer",
+			reason: "suka aja",
+		},
+		{
+			name: "Jaka",
+			alamat: "jakarta",
+			pekerjaan: "developer",
+			reason: "suka aja",
+		},
+		{
+			name: "Wicaksana",
+			alamat: "jakarta",
+			pekerjaan: "developer",
+			reason: "suka aja",
+		},
+	}
+	args := os.Args
+	indexArgs := args[1:]
+	index, _ := strconv.Atoi(indexArgs[0])
+	for i, student := range students {
+		if i == index - 1 {
+			fmt.Println(student)
+		}
+	}
+}
